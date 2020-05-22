d6a1dbfdbdc0266c4a7b3e2bea86044813390dc8
Glenn Posadas <hello@glennvon.com>
2020-05-23 02:56:11 +0800

    add views and update podfile to ios.

--- StripeExample-iOS/StripeExample/Controllers/HomeViewController.swift
+++ StripeExample-iOS/StripeExample/Controllers/HomeViewController.swift
@@ -6,25 +6,24 @@
 //  Copyright © 2020 GAM Dynamics. All rights reserved.
 //
 
+import Kingfisher
+import Stripe
 import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var bannerImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func purchaseTapped(_ sender: Any) {
+        
     }
-    */
-
+    
 }
